58525fc4e4b8079334c0bfe89ac16ec98561e83f
hendarSu <hendarsundana@gmail.com>
2018-12-12 02:35:34 +0700

    chapter03 initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BeersModule } from './beers/beers.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BeersModule
   ],
   providers: [],
   bootstrap: [AppComponent]
